fb004c2f94bd7ed97932551cc4b175f89a828212
Stas <stsgolovnev@gmail.com>
2023-01-04 15:06:10 +0300

    Начал добавление логина, исправил первую ошибку

--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('task-create', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
-    path('login/', MyLoginView, name='login')
+    path('login/', MyLoginView.as_view, name='login')
 ]
